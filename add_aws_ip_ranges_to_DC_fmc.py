17cd046b41d3b300c24d9c2a82c6498fe321621a
alex-boley <alex.boley@ithaka.org>
2022-01-28 10:33:08 -0500

    new DC FMC files

--- add_aws_ip_ranges_to_DC_fmc.py
+++ add_aws_ip_ranges_to_DC_fmc.py
@@ -260,6 +260,8 @@ def put_new_aws_east(object_type, objectname):
             json_resp = json.loads(resp)
         else :
             r.raise_for_status()
+            json_resp = json.loads(resp)
+            print(json_resp)
     except requests.exceptions.HTTPError as err:
         print ("Error in connection --> "+str(err))
     finally:
